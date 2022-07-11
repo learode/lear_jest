121ff90ded3b3f5203e767616c9ad73aae05ab9d
mulfranck <mulfranck5@gmail.com>
2022-07-11 06:56:44 +0100

    caesars cipher tests pass

--- caesarsCipher/caesarsCipher.js
+++ caesarsCipher/caesarsCipher.js
@@ -1,20 +1,29 @@
 function rot13(str) {
     let len = str.length;
-    let decript = '';
+    let encript = '';
     let charCode = null, position = null;
 
     for (let i = 0; i < len; i++) {
         charCode = str.charCodeAt(i);
-        position = charCode + 13;
 
+        if (charCode > 90 || charCode < 65) {
+            encript += String.fromCharCode(charCode);
+            continue;
+
+        }
+
+
+        position = charCode + 13;
         if (position > 90) {
             position -= 91
             position += 65
         }
-        decript += String.fromCharCode(position);
+        
+        
+        encript += String.fromCharCode(position);
     }
 
-    return decript;
+    return (encript);
 }
 
 export default rot13;
--- caesarsCipher/caesarsCipher.test.js
+++ caesarsCipher/caesarsCipher.test.js
@@ -13,4 +13,20 @@ describe('caesarsCipher test cases : ', () => {
     test('X should equal J', () => {
         expect(caesarsCipher('X')).toMatch(/K/);
     })
+
+    test('AB X should equal NO K', () => {
+        expect(caesarsCipher('AB K')).toMatch(/NO X/);
+    })
+    test('STUDY HARD! should e FGHXL UNEQ!', () => {
+        expect(caesarsCipher('STUDY HARD!')).toMatch(/FGHQL UNEQ!/);
+    })
+    test('@ZHYSENAPX. should equal @MULFRANCK.', () => {
+        expect(caesarsCipher('@ZHYSENAPX.')).toMatch(/@MULFRANCK./);
+    })
+});
+
+describe('Now this works', () => {
+    test('Encrypt "THIS NIGGA (@_@)"', () => {
+        expect(caesarsCipher('THIS NIGGA (@_@)')).toMatch(/./g);
+    })
 })
