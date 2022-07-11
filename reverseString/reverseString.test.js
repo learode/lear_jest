35e05bceb4a4caeaa0ff741299732b5c72358b6b
mulfranck <mulfranck5@gmail.com>
2022-07-11 07:16:27 +0100

    pass reverseString

--- reverseString/reverseString.test.js
+++ reverseString/reverseString.test.js
@@ -4,13 +4,13 @@ describe('Reverse String test', () => {
     test('mul should equal lum', () => {
         expect(reverseStr('mul')).toMatch(/lum/);
     })
-    test.skip('franck should equal kcnarf', () => {
+    test('franck should equal kcnarf', () => {
         expect(reverseStr('franck')).toMatch(/kcnarf/);
     })
-    test.skip('lumkcnarf should equal mulfranck', () => {
-        expect(reverseStr('lumkcnarf')).toMatch(/mulfranck/);
+    test('kcnarflum should equal mulfranck', () => {
+        expect(reverseStr('kcnarflum')).toMatch(/mulfranck/);
     })
-    test.skip('to go should equal og ot', () => {
+    test('to go should equal og ot', () => {
         expect(reverseStr('to go')).toMatch(/og ot/);
     })
 })
