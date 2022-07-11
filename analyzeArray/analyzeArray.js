5917810bc5eb9d7e0e8ed1aec4956ed6ab4bf42b
mulfranck <mulfranck5@gmail.com>
2022-07-11 07:54:21 +0100

    all pass analyseArray test

--- analyzeArray/analyzeArray.js
+++ analyzeArray/analyzeArray.js
@@ -1,6 +1,19 @@
-(function analyzer(arr){
+export default function analyser(arr){
     let min= null, max = null, len = null, avg = null, sum = null;
     
+    min = arr[0];
+    len = arr.length;
 
-    console.log({min, max, len, avg, sum});
-})([1, 2, 3, 4, 5]);
+    sum = arr.reduce((toal, current) => toal + current)
+    avg = +(sum/len).toFixed(2)
+
+    for (let i = 0; i < len; i++) {
+        if (arr[i] < min) {
+            min = arr[i]
+        }
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+    }
+    return {min, max, len, avg, sum};
+};
--- analyzeArray/analyzeArray.test.js
+++ analyzeArray/analyzeArray.test.js
@@ -2,16 +2,16 @@ import analyser from './analyzeArray';
 
 describe('AnalyzeArray Test', () => {
     test('[1, 2, 3, 4, 5] return ', () => {
-        expect([1, 2, 3, 4, 5]).toBe({
+        expect(analyser([1, 2, 3, 4, 5])).toEqual({
             min: 1,
             max: 5,
             len: 5,
             sum: 15,
-            avg: 5,
+            avg: 3,
         })
     })
     test('[-6, 8, 5] return ', () => {
-        expect([-6, 8, 5]).toBe({
+        expect(analyser([-6, 8, 5])).toEqual({
             min: -6,
             max: 8,
             len: 3,
